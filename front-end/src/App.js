b7883d616e6971fdd276d29c6974ede30540d1d1
Zanoni <zanoni.odenir@gmail.com>
2022-06-02 08:43:24 -0300

    feat: criação da rota /login, passando no test 01

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+      <Navigate to="/login" replace />
     </div>
   );
 }
