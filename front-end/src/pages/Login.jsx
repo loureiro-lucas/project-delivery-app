ed2b7438443c3607a182868d3ef9927c48e554f7
loureiro-lucas <lucas-loureiro@outlook.com>
2022-06-05 18:47:16 -0300

    refactor: estiliza a tela de cadastro, ajusta a responsividade e move validações  para a pasta utils

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -5,21 +5,15 @@ import { useNavigate } from 'react-router-dom';
 import Box from '../components/Box';
 import Button from '../components/Button';
 import Container from '../components/Container';
+import ErrorMessage from '../components/ErrorMessage';
 import Form from '../components/Form';
 import Input from '../components/Input';
 import Logo from '../components/Logo';
 import logo from '../images/rockGlass.svg';
 
+import { validateEmailInput, validatePasswordInput } from '../utils/inputValidations';
+
 import { postLogin } from '../services';
-
-export const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-export const PASSWORD_MIN_LENGTH = 6;
-
-export const validatePasswordInput = (passwordInput) => {
-  if (passwordInput.length >= PASSWORD_MIN_LENGTH) return true;
-  return false;
-};
-export const validateEmailInput = (loginInput) => loginInput.match(EMAIL_REGEX);
 
 export default function Login() {
   const [loginInput, setLoginInput] = useState('');
@@ -62,47 +56,48 @@ export default function Login() {
 
   return (
     <Container>
-      <Box>
+      <Box style={ { marginTop: '10vh' } }>
         <Logo src={ logo } alt="logo com um copo" />
-        <Box style={ { width: '350px' } }>
-          <Form
-            onSubmit={ submitLogin }
-            style={ { flexDirection: 'column', height: '280px' } }
+        <Form
+          onSubmit={ submitLogin }
+          style={ { flexDirection: 'column', height: '300px' } }
+        >
+          <span style={ { padding: '0 15px' } }>Login</span>
+          <Input
+            data-testid="common_login__input-email"
+            onChange={ handleEmailInput }
+            placeholder="email@trybeer.com.br"
+            type="text"
+            value={ loginInput }
+          />
+          <span style={ { padding: '0 5px' } }>Senha</span>
+          <Input
+            data-testid="common_login__input-password"
+            onChange={ handlePasswordInput }
+            placeholder="********"
+            type="password"
+            value={ passwordInput }
+          />
+          { alarmErrorLogin && (
+            <ErrorMessage data-testid="common_login__element-invalid-email">
+              Credenciais inválidas
+            </ErrorMessage>
+          )}
+          <Button
+            data-testid="common_login__button-login"
+            disabled={ isButtonDisabled }
+            type="submit"
           >
-            <span style={ { padding: '0 15px' } }>Login</span>
-            <Input
-              data-testid="common_login__input-email"
-              onChange={ handleEmailInput }
-              placeholder="email@trybeer.com.br"
-              type="text"
-              value={ loginInput }
-            />
-            <span style={ { padding: '0 5px' } }>Senha</span>
-            <Input
-              data-testid="common_login__input-password"
-              onChange={ handlePasswordInput }
-              placeholder="********"
-              type="password"
-              value={ passwordInput }
-            />
-            <Button
-              data-testid="common_login__button-login"
-              disabled={ isButtonDisabled }
-              type="submit"
-            >
-              LOGIN
-            </Button>
-            <Button
-              data-testid="common_login__button-register"
-              onClick={ () => navigate('/register') }
-            >
-              Ainda não tenho conta
-            </Button>
-          </Form>
-        </Box>
+            LOGIN
+          </Button>
+          <Button
+            data-testid="common_login__button-register"
+            onClick={ () => navigate('/register') }
+          >
+            Ainda não tenho conta
+          </Button>
+        </Form>
       </Box>
-      { alarmErrorLogin
-        ? <p data-testid="common_login__element-invalid-email">erro de login</p> : null }
     </Container>
   );
 }
